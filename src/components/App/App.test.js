8948a62c8954ce95fa607189834e62cd7e9089cc
Anna <anja_kaa@mail.ru>
2018-04-03 22:23:57 +0300

    homeWork_1

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import {shallow} from 'enzyme';
+import App from '../App';
 
-const wrapper = shallow(<App />);
+const wrapper = shallow(<App/>);
 
 describe('Домашнее задание', () => {
   describe('Рендер компоненты App', () => {
