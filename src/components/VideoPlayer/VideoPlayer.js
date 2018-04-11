2834b42f729e749b7d011410fa4c55cef08787c9
Pasha_Ree <p.reshetnev@yandex.ru>
2018-04-11 09:47:25 +0300

    presave_cor_prev

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -3,8 +3,32 @@ import videoFile from './Video.mp4';
 import './VideoPlayer.css';
 
 class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.video = React.createRef();
+  }
+
+  handleClick = (e) => {
+    if (e.target.value === 'Play') {
+      this.video.play();
+    } else {
+      this.video.pause();
+    }
+  }
+
   render() {
-    return null;
+    return (
+      <div className='video-player'>
+        <video className='video-player__source' ref={this.video}>
+          <source src={videoFile} type='video/mp4'/>
+        </video>
+        <div>
+          <button onClick={this.handleClick}>Play</button>
+          <button onClick={this.handleClick}>Stop</button>
+        </div>
+      </div>
+    );
   }
 }
 
