f247d016996a27dd640068032d546969fcbc4b80
Andrey Kulakovski <a.kulakovski@blak-it.com>
2018-04-15 19:58:17 +0300

    Add hw1

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='description'>
+        Миру-мир, студентам - beer.
+      </p>
+    </div>
+  )
+}
+
+export default App
