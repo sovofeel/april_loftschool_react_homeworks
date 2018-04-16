11e64f44988c2d7531b0e9dddf0942fb439aaa38
Anna <anja_kaa@mail.ru>
2018-04-16 22:19:13 +0300

    homework_5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,34 @@
 import React, { PureComponent } from 'react';
-import { Switch, withRouter } from 'react-router-dom';
+import { Switch, withRouter, Link, Route } from 'react-router-dom';
 import { AuthorizeProvider } from 'components/AuthorizeProvider';
+import Login from '../Login';
+import PrivateRoute from '../PrivateRoute';
+import Private from '../Private';
+import Public from '../Public';
 
 export class App extends PureComponent {
   render() {
     return (
       <AuthorizeProvider>
         <div>
-          <Switch />
+          <nav>
+            <ul>
+              <li>
+                <Link to={`/login`}>Войти</Link>
+              </li>
+              <li>
+                <Link to={`/private`}>Секретная страница</Link>
+              </li>
+              <li>
+                <Link to={`/`}>Главная</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/" exact component={Public} />
+            <Route path="/login" component={Login} />
+            <PrivateRoute pathName="/private" component={Private} />
+          </Switch>
         </div>
       </AuthorizeProvider>
     );
--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,14 @@
 import React, { PureComponent } from 'react';
 import { AuthHOC } from 'components/AuthorizeProvider';
+import { Route, Redirect } from 'react-router-dom';
 
 class PrivateRoute extends PureComponent {
   render() {
-    return null;
+    const { isAuthorized, pathName, component } = this.props;
+    {
+      if (isAuthorized) return <Route path={pathName} exact component={component} />;
+      else return <Redirect from={pathName} to="/login" />;
+    }
   }
 }
 
